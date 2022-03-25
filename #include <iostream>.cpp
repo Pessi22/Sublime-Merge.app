206399790ea2dec3a3119f44f6cd4ed7a18cf19d
Serhii Kryvyi <serhii.kryvyi1807@gmail.com>
2022-03-26 00:58:51 +0200

    over

---
+++ #include <iostream>.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Called function std::cout
+void print()
+{
+    //Called operator << na obiekt tipa std::cout
+    std::cout << "Hello Skillbox!\n";
+}
+/*
+* pechataem hello world!
+*/
+int main()
+{
+    int x = 100;
+    3 + 7;
+    int y = x + 100;
+
+    int b = 0;
+    b = b + 2;
+
+    int test;
+    test = 100500;
+
+    int test2 = 1005001;
+
+    
+    int mult = x * y;
+    int random = 1002;
+
+    std::cout << random;
+}
